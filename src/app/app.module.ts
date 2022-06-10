ff8ca4873d21c361df2956048c8e6d607bb83b18
Martin Koroudjiev <martin@prosveta.bg>
2022-06-10 19:46:31 +0300

    Martin <mrtndimitrov@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxFabricjsWhiteboardModule } from "ngx-fabricjs-whiteboard";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxFabricjsWhiteboardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
