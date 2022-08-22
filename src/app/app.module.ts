6c2fba33146fc6613395fb004e29884111ab7c9a
Martin <martin@prosveta.bg>
2022-08-22 15:16:58 +0300

    Martin <mrtndimitrov@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NgxFabricjsWhiteboardModule } from "ngx-fabricjs-whiteboard";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
